f7c4bb77a2e4beff31feb7d643dd5c4a0b5b61a6
gerry <gerryyhonatan29@gmail.com>
2025-05-07 19:38:10 +0700

    feat: data table UI

--- src/components/ui/DataTable/DataTable.tsx
+++ src/components/ui/DataTable/DataTable.tsx
@@ -1,16 +1,70 @@
-import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
-import { Key, ReactNode } from "react";
+import { LIMIT_LISTS } from "@/constants/list.constants";
+import { cn } from "@/utils/cn";
+import { Button, Input, Pagination, Select, SelectItem, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import { em } from "framer-motion/client";
+import { ChangeEvent, Key, ReactNode, useMemo } from "react";
+import { CiSearch } from "react-icons/ci";
 
 interface PropTypes {
     columns: Record<string, unknown>[];
     data: Record<string, unknown>[];
     renderCell: (item: Record<string, unknown>, columnKey: Key) => ReactNode;
+    onClearSearch: () => void;
+    onChangeSearch: (e: ChangeEvent<HTMLInputElement>) => void;
+    buttonTopContentLabel?: string;
+    onClickButtonTopContent?: () => void;
+    limit: string;
+    currentPage: number;
+    onChangeLimit: (e: ChangeEvent<HTMLSelectElement>) => void;
+    onChangePage: (page: number) => void;
+    totalPages: number;
+    emptyContent: string;
+    isLoading?: boolean;
 }
 
 const DataTable = (props: PropTypes) => {
-    const {columns, data, renderCell} = props;
+    const {columns, data, renderCell, onClearSearch, onChangeSearch, buttonTopContentLabel, onClickButtonTopContent, limit, onChangeLimit, currentPage, totalPages, onChangePage, emptyContent, isLoading} = props;
+
+    const TopContent = useMemo(() => {
+        return (
+            <div className="flex lg:flex-row flex-col-reverse items-start justify-between gap-y-4 lg:items-center">
+                <Input isClearable className="w-full sm:max-w-[24%]" placeholder="Search by name" startContent={<CiSearch/>} onClear={onClearSearch} onChange={onChangeSearch}/>
+                {buttonTopContentLabel && (
+                    <Button color="primary" onPress={onClickButtonTopContent}>
+                        {buttonTopContentLabel}
+                    </Button>
+                )}
+            </div>
+        )
+    }, [buttonTopContentLabel, onClearSearch, onChangeSearch, onClickButtonTopContent]);
+
+    const BottomContent = useMemo(() => {
+        return (
+            <div className="flex items-center justify-center px-2 py-2 lg:justify-between">
+                <Select 
+                className="hidden max-w-36 lg:block" 
+                size="sm" 
+                selectedKeys={[limit]} 
+                selectionMode="single" 
+                onChange={onChangeLimit} 
+                startContent={<p className="text-small">Show:</p>}
+                >
+                    {LIMIT_LISTS.map((item) => (
+                        <SelectItem key={item.value} value={item.value}>
+                            {item.label}
+                        </SelectItem>
+                    ))}
+                </Select>
+                <Pagination isCompact showControls color="primary" page={currentPage} total={totalPages} onChange={onChangePage}/>
+            </div>
+        )
+    },[limit, currentPage, totalPages, onChangePage, onChangeLimit]);
+
     return (
-        <Table>
+        <Table topContent={TopContent} topContentPlacement="inside" bottomContent={BottomContent} bottomContentPlacement="inside" classNames={{
+            base: "max-w-full",
+            wrapper: cn({"overflow-x-hidden": isLoading})
+        }}>
             <TableHeader columns={columns}>
                 {(column) => (
                     <TableColumn key={column.uid as Key}>
@@ -19,7 +73,10 @@ const DataTable = (props: PropTypes) => {
                 )}
             </TableHeader>
 
-            <TableBody items={data}>
+            <TableBody items={data} emptyContent={emptyContent} isLoading={isLoading} loadingContent={
+                <div className="flex h-full w-full items-center justify-center bg-foreground-700/30 backdrop-blur">
+                    <Spinner color="primary"/>
+                </div>}>
                 {(item) => (
                     <TableRow key={item._id as Key}>
                         {(columnKey) => (
--- src/components/views/Admin/ProductSetup/ProductSetup.tsx
+++ src/components/views/Admin/ProductSetup/ProductSetup.tsx
@@ -5,6 +5,7 @@ import { Key, ReactNode, useCallback } from "react";
 import { CiMenuKebab } from "react-icons/ci";
 import { COLUMN_LISTS_BRAND, COLUMN_LISTS_CATEGORY } from "./Category.constant";
 import { desc } from "framer-motion/client";
+import { LIMIT_LISTS } from "@/constants/list.constants";
 
 const ProductSetup = () => {
     const {push} = useRouter()
@@ -28,30 +29,31 @@ const ProductSetup = () => {
                 return cellValue as ReactNode;
         }
     }, [push]);
+
     return (
         <Tabs aria-label="Product Setup">
-            <Tab key="Brands" title="Brands">
+            <Tab key="Categories" title="Categories">
             <section>
-            <DataTable renderCell={renderCell} columns={COLUMN_LISTS_CATEGORY} data={[
+            <DataTable onChangeSearch={() => {}} onClearSearch={() => {}} buttonTopContentLabel="Create Category" onClickButtonTopContent={() => {}} limit={LIMIT_LISTS[0].label} onChangeLimit={() => {}} currentPage={1} onChangePage={() => {}} totalPages={2} renderCell={renderCell} columns={COLUMN_LISTS_CATEGORY} emptyContent="Category is empty" data={[
                 {
                     _id: "123",
                     name: "Category 1",
                     description: "Description 1",
                 }
-            ]}></DataTable>
-        </section>
+            ]}/>
+            </section>
             </Tab>
-            <Tab key="Categories" title="Categories">
+            <Tab key="Brands" title="Brands">
             <section>
-            <DataTable renderCell={renderCell} columns={COLUMN_LISTS_BRAND} data={[
+            <DataTable onChangeSearch={() => {}} onClearSearch={() => {}} buttonTopContentLabel="Add New Brand" onClickButtonTopContent={() => {}} limit={LIMIT_LISTS[0].label} onChangeLimit={() => {}} currentPage={1} onChangePage={() => {}} totalPages={2} renderCell={renderCell} columns={COLUMN_LISTS_BRAND} emptyContent="Brand is empty" data={[
                 {
                     _id: "123",
                     icon: "../../../public/images/login.png",
-                    name: "Category 1",
-                    description: "Description 1",
+                    name: "Brand 1",
+                    description: "Brand 1",
                 }
             ]}></DataTable>
-        </section>
+            </section>
             </Tab>
         </Tabs>
         
